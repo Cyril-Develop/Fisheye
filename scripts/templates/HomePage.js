f30915daa85a8519b674e633ca80905fd27cb354
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2024-01-24 10:30:41 +0100

    modif design

--- scripts/templates/HomePage.js
+++ scripts/templates/HomePage.js
@@ -4,17 +4,21 @@ export default class PhotographerCard {
     }
 
     createPhotographerCard() {
-        const article = document.createElement( 'article' );
+        const article = document.createElement( 'a' );
+        article.setAttribute("aria-label", `Voir le profil de ${this.photographer.name}`);
+        article.setAttribute("role", "link");
+        article.href = `photographer.html?id=${this.photographer.id}`;
         const photographerCard = `
-            <a href="photographer.html?id=${this.photographer.id}" role="link" aria-label="Voir le profil de ${this.photographer.name}">
+            <article class="photographer_card">
                 <img class="photographer_thumbnail" src="./assets/images/photographers/thumbnails/${this.photographer.portrait}" alt="${this.photographer.name}">
                 <h2 class="photographer_name">${this.photographer.name}</h2>
-            </a>
-            <p class="photographer_location">${this.photographer.city}, ${this.photographer.country}</p>
-            <p class="photographer_tagline">${this.photographer.tagline}</p>
-            <span class="photographer_price">${this.photographer.price}€/jour</span>
+                <p class="photographer_location">${this.photographer.city}, ${this.photographer.country}</p>
+                <p class="photographer_tagline">${this.photographer.tagline}</p>
+                <span class="photographer_price">${this.photographer.price}€/jour</span>
+            </article>
+                
         `;
         article.innerHTML = photographerCard;
-        return article;
+        return article; 
     }
 }
